91ad999919a36c8958b6f7c061c96ec2746c2c8a
aereeeee <dofl5576@gmail.com>
2019-10-04 21:45:03 +0900

    feat: fix db connection to pool connection
    checks database sql 수정

--- src/database/checks.js
+++ src/database/checks.js
@@ -1,17 +1,13 @@
 const createError = require("http-errors");
-const database = require("../database/database");
+const pool = require("./db");
 const checks = require("../model/checks");
 
 const createCheck = async function(userKey, listId) {
-  const conn = database.createConnection();
-
   const sql1 = `INSERT INTO Checks (user_key, list_id) VALUES ('${userKey}', '${listId}');`;
-  const result = await database.query(conn, sql1);
+  const result = await pool.query(sql1);
 
   const sql2 = `SELECT LAST_INSERT_ID() AS id;`;
-  const result2 = await database.query(conn, sql2);
-
-  database.endConnection(conn);
+  const result2 = await pool.query(sql2);
 
   const checkId = result2[0]["id"];
   return await getCheck(checkId);
@@ -19,7 +15,7 @@ const createCheck = async function(userKey, listId) {
 
 const getCheck = async function(checkId) {
   const sql = `SELECT * FROM Checks WHERE id = ${checkId}`;
-  const checkResult = await database.queryOne(sql);
+  const checkResult = await pool.query(sql);
 
   if (checkResult.length == 0) {
     throw createError(404, `There is no checks with check Id is ${checkId}`);
@@ -30,7 +26,7 @@ const getCheck = async function(checkId) {
 
 const getListUser = async function(userKey, listId) {
   const sql = `SELECT * FROM Checks WHERE list_id = ${listId} AND user_key = ${userKey}`;
-  const checkResult = await database.queryOne(sql);
+  const checkResult = await pool.query(sql);
 
   if (checkResult.length == 0) {
     throw createError(
@@ -45,7 +41,7 @@ const getListUser = async function(userKey, listId) {
 const getListUsers = async function(listId) {
   const conn = database.createConnection();
   const sql = `SELECT * FROM Checks WHERE list_id = ${listId}`;
-  const checkResult = await database.query(conn, sql);
+  const checkResult = await pool.query(sql);
 
   if (checkResult.length == 0) {
     throw createError(404, `There is no users with list Id is ${listId}`);
@@ -73,7 +69,7 @@ const modifyCheck = async function(userKey, listId, want, like, lists) {
   const sql = `UPDATE Checks SET ${queries.join(
     ", "
   )} WHERE user_key = ${userKey} AND list_id = ${listId};`;
-  const result = await database.queryOne(sql);
+  const result = await pool.query(sql);
   const result2 = await lists.modifyList(
     listId,
     undefined,
@@ -88,11 +84,9 @@ const modifyCheck = async function(userKey, listId, want, like, lists) {
 };
 
 const deleteCheck = async function(userKey, listId) {
-  const conn = database.createConnection();
-
   const sql1 = `DELETE FROM Checks WHERE user_key = ${userKey} AND list_id = ${listId};`;
 
-  const result1 = await database.query(conn, sql1);
+  const result1 = await pool.query(sql1);
 
   return result1;
 };
