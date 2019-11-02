5118dee931f62cd64b2d85ee9ee0eca0b136a3f3
aereeeee <dofl5576@gmail.com>
2019-11-02 22:05:01 +0900

    refactor: add error handling to  checks database logic

--- src/database/checks.js
+++ src/database/checks.js
@@ -15,13 +15,15 @@ const createCheck = async function(userKey, listId) {
 
 const getCheck = async function(checkId) {
   const sql = `SELECT * FROM Checks WHERE id = ${checkId}`;
-  const [checkResult] = await pool.execute(sql);
-
-  if (checkResult.length == 0) {
-    throw createError(404, `There is no checks with check Id is ${checkId}`);
+  try {
+    const [checkResult] = await pool.execute(sql);
+    if (checkResult.length == 0) {
+      throw createError(404, `There is no checks with check Id is ${checkId}`);
+    }
+    return await checks.convertToCheck(checkResult[0]);
+  } catch (e) {
+    throw createError(e);
   }
-
-  return await checks.convertToCheck(checkResult[0]);
 };
 
 const checkListUser = async function(userKey, listId) {
@@ -38,31 +40,38 @@ const checkListUser = async function(userKey, listId) {
 
 const getListUser = async function(userKey, listId) {
   const sql = `SELECT * FROM Checks WHERE list_id = ${listId} AND user_key = ${userKey}`;
-  const [checkResult] = await pool.execute(sql);
-  if (checkResult.length == 0) {
-    throw createError(
-      404,
-      `There is no users with list Id is ${listId} and user Key is ${userKey}`
-    );
+  try {
+    const [checkResult] = await pool.execute(sql);
+    if (checkResult.length == 0) {
+      throw createError(
+        404,
+        `There is no users with list Id is ${listId} and user Key is ${userKey}`
+      );
+    }
+    return await checks.convertToCheck(checkResult[0]);
+  } catch (e) {
+    throw createError(e);
   }
-
-  return await checks.convertToCheck(checkResult[0]);
 };
 
 const getListUsers = async function(listId) {
   const sql = `SELECT * FROM Checks WHERE list_id = ${listId}`;
-  const [checkResult] = await pool.execute(sql);
+  try {
+    const [checkResult] = await pool.execute(sql);
 
-  if (checkResult.length == 0) {
-    throw createError(404, `There is no users with list Id is ${listId}`);
+    if (checkResult.length == 0) {
+      throw createError(404, `There is no users with list Id is ${listId}`);
+    }
+
+    const result = [];
+    for (let i = 0; i < checkResult.length; i++) {
+      result.push(checks.convertToCheck(checkResult[i]));
+    }
+
+    return await result;
+  } catch (e) {
+    throw createError(e);
   }
-
-  const result = [];
-  for (let i = 0; i < checkResult.length; i++) {
-    result.push(checks.convertToCheck(checkResult[i]));
-  }
-
-  return await result;
 };
 
 // 0 또는 1의 값만 받아온다.
@@ -79,26 +88,33 @@ const modifyCheck = async function(userKey, listId, want, like, lists) {
   const sql = `UPDATE Checks SET ${queries.join(
     ", "
   )} WHERE user_key = ${userKey} AND list_id = ${listId};`;
-  const result = await pool.execute(sql);
-  const result2 = await lists.modifyList(
-    listId,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    1,
-    1
-  );
 
-  return await getListUser(userKey, listId);
+  try {
+    const result = await pool.execute(sql);
+    const result2 = await lists.modifyList(
+      listId,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      1,
+      1
+    );
+
+    return await getListUser(userKey, listId);
+  } catch (e) {
+    throw createError(e);
+  }
 };
 
 const deleteCheck = async function(userKey, listId) {
   const sql1 = `DELETE FROM Checks WHERE user_key = ${userKey} AND list_id = ${listId};`;
-
-  const result1 = await pool.execute(sql1);
-
-  return result1;
+  try {
+    const result1 = await pool.execute(sql1);
+    return result1;
+  } catch (e) {
+    throw createError(e);
+  }
 };
 
 module.exports = {
